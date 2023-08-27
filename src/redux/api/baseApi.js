671c73ef27bf65517a487dad7771476b83594e0e
ARahmanLskit <arahman@lskit.com>
2023-08-27 17:55:20 +0600

    base url updated

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BASE_URL + "api/v1" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://admin.alibainternational.com/" + "api/v1" }),
     tagTypes: [],
     endpoints: (builder) => ({}),
 });
