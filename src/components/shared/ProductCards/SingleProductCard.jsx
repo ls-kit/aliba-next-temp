5d47eddab2f6ef1f5d744250e2fb5c36185a5de7
ARahmanLskit <arahman@lskit.com>
2023-08-28 17:15:45 +0600

    single product card title design update

--- src/components/shared/ProductCards/SingleProductCard.jsx
+++ src/components/shared/ProductCards/SingleProductCard.jsx
@@ -6,7 +6,7 @@ import Rating from "react-rating";
 
 const SingleProductCard = ({ data }) => {
     // console.log(data);
-    const { img, name, rating, sale_price, product_code, total_sold } = data;
+    const { img, name, rating, sale_price, product_code, total_sold,stock } = data;
     return (
         <Link href={`/product/${product_code}`} className="col-12 col-md-3">
             <div className="collection-product-wrapper">
@@ -47,7 +47,15 @@ const SingleProductCard = ({ data }) => {
 
                         <div className="product-detail p-3">
                             <div>
-                                <h6>SOLD:{total_sold}</h6>
+                                <div  className="d-flex flex-row align-items-center  justify-content-between">
+
+                                <h6 className="text-black font-bold">
+                                Sold:  {total_sold}
+                                </h6>
+                                <h6 className="text-black font-bold">
+                                stock:   {stock}
+                                </h6>
+                                </div>
                             </div>
                             <div className="rating">
                                 <Rating
@@ -58,7 +66,7 @@ const SingleProductCard = ({ data }) => {
                                 />
                             </div>
                             <div>
-                                <h6>{name}</h6>
+                                <h6>{name.slice(0,35)}...</h6>
                             </div>
 
                             <h4>৳{sale_price}</h4>
