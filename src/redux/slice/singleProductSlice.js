ec6ac1096e74dae5e9809f0863510b3ff13efcbb
ARahmanLskit <arahman@lskit.com>
2023-08-29 19:12:33 +0600

    redux toolkit slice added

---
+++ src/redux/slice/singleProductSlice.js
@@ -0,0 +1,42 @@
+const { createSlice } = require("@reduxjs/toolkit")
+
+
+
+const initialState = {
+    item: {
+        
+    },
+    totalQuantity: 0,
+    totalPrice: 0,
+    totalWeight: 0,
+    shippingCost: 0,
+    chinaLocalShippingCost: 0,
+    shippingCostPerKG: 0,
+    deliveryDate: '',
+    variant: [
+        {
+            colorName: '',
+            price: 0,
+            size: 0,
+            quantity: 0,
+        }
+    ]
+
+}
+
+const singleProductSlice = createSlice({
+    name: "singleProduct",
+    initialState,
+    reducers: {
+        increaseQuantity: (state, action) => {
+            
+        },
+        decreaseQuantity: (state, action) => {
+            
+        }
+    }
+})
+
+export const { increaseQuantity, decreaseQuantity } = singleProductSlice.actions;
+
+export default singleProductSlice.reducer;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import adminSliceReducer from '../features/admin/adminSlice';
 import { apiSlice } from "../api/baseApi";
-// import projectsSliceReducer from '../features/projects/projectsSlice';
+import singleProductReducer from "../slice/singleProductSlice";
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         // projectsSlice: projectsSliceReducer,
         // adminSlice: adminSliceReducer,
+        singleProductSlice: singleProductReducer
     },
     devTools: process.env.NODE_ENV !== "production",
 
